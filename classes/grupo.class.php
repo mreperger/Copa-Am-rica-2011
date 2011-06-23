2888d4b2a6c883333463ad24858b24b10fb05d26
Marcelo Reperger <drakeshmok@gmail.com>
2011-06-23 00:21:51 -0300

    se agregan funciones para ordenar los grupos

--- classes/grupo.class.php
+++ classes/grupo.class.php
@@ -19,6 +19,110 @@
 			}
 			
 			//Ordeno array segun reglas de puntos
+			$this->ordenarGrupo();
+		}
+		
+		private function ordenarGrupo(){
+			//Ordeno por pts (mayor a menor)
+			$this->ordenarGrupoPTS();
+			//Ordeno por ga (mayor a menor)
+			$this->ordenarGrupoGA();
+			//Ordeno por gc (mayor a menor)
+			$this->ordenarGrupoGC();
+			//Ordeno por ge (menor a mayor)
+			$this->ordenarGrupoGE();
+			//Ordeno por nombre (menor a mayor)
+			$this->ordenarGrupoNOMBRE();
+		}
+		
+		private function ordenarGrupoPTS(){
+			$n = count($this->equipos);
+			do{
+				$newn = 0;
+				for($i = 0; $i < $n-1; $i++){
+					$equipo1 = $this->equipos[$i];
+					$equipo2 = $this->equipos[$i+1];
+					if($equipo1->getPTS() < $equipo2->getPTS()){
+						//CAMBIO POS
+						$this->equipos[$i] = $equipo2;
+						$this->equipos[$i + 1] = $equipo1;
+						$newn = $i + 1;
+					}
+				}
+				$n = $newn;
+			}while($n > 1);
+		}
+		
+		private function ordenarGrupoGA(){
+			$n = count($this->equipos);
+			do{
+				$newn = 0;
+				for($i = 0; $i < $n-1; $i++){
+					$equipo1 = $this->equipos[$i];
+					$equipo2 = $this->equipos[$i+1];
+					if($equipo1->getPTS() == $equipo2->getPTS() && $equipo1->getGA() < $equipo2->getGA()){
+						//CAMBIO POS
+						$this->equipos[$i] = $equipo2;
+						$this->equipos[$i + 1] = $equipo1;
+						$newn = $i + 1;
+					}
+				}
+				$n = $newn;
+			}while($n > 1);
+		}
+		
+		private function ordenarGrupoGC(){
+			$n = count($this->equipos);
+			do{
+				$newn = 0;
+				for($i = 0; $i < $n-1; $i++){
+					$equipo1 = $this->equipos[$i];
+					$equipo2 = $this->equipos[$i+1];
+					if($equipo1->getPTS() == $equipo2->getPTS() && $equipo1->getGA() == $equipo2->getGA() && $equipo1->getGC() < $equipo2->getGC()){
+						//CAMBIO POS
+						$this->equipos[$i] = $equipo2;
+						$this->equipos[$i + 1] = $equipo1;
+						$newn = $i + 1;
+					}
+				}
+				$n = $newn;
+			}while($n > 1);
+		}
+		
+		private function ordenarGrupoGE(){
+			$n = count($this->equipos);
+			do{
+				$newn = 0;
+				for($i = 0; $i < $n-1; $i++){
+					$equipo1 = $this->equipos[$i];
+					$equipo2 = $this->equipos[$i+1];
+					if($equipo1->getPTS() == $equipo2->getPTS() && $equipo1->getGA() == $equipo2->getGA() && $equipo1->getGC() == $equipo2->getGC && $equipo1->getGE() > $equipo2->getGE()){
+						//CAMBIO POS
+						$this->equipos[$i] = $equipo2;
+						$this->equipos[$i + 1] = $equipo1;
+						$newn = $i + 1;
+					}
+				}
+				$n = $newn;
+			}while($n > 1);
+		}
+		
+		private function ordenarGrupoNOMBRE(){
+			$n = count($this->equipos);
+			do{
+				$newn = 0;
+				for($i = 0; $i < $n-1; $i++){
+					$equipo1 = $this->equipos[$i];
+					$equipo2 = $this->equipos[$i+1];
+					if($equipo1->getPTS() == $equipo2->getPTS() && $equipo1->getGA() == $equipo2->getGA() && $equipo1->getGC() == $equipo2->getGC && $equipo1->getGE() == $equipo2->getGE() && $equipo1->getNombre() > $equipo2->getNombre()){
+						//CAMBIO POS
+						$this->equipos[$i] = $equipo2;
+						$this->equipos[$i + 1] = $equipo1;
+						$newn = $i + 1;
+					}
+				}
+				$n = $newn;
+			}while($n > 1);
 		}
 		
 		public function getEquipos(){
