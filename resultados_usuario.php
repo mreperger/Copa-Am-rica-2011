54e695feb5c0e64b47cfeb5ac5fac3b18c0c189e
Mariano Reperger <mariano@reperger.com>
2011-06-27 00:22:06 -0300

    Se borra la funcion de estadio

--- resultados_usuario.php
+++ resultados_usuario.php
@@ -1,17 +1,7 @@
 <?php session_start(); ?>
 <?php require_once("includes/ini.php"); ?>
 <?php require_once("classes/equipo.class.php"); ?>
-<?php
-	function getNombreEstadio($id,$conn){
-		$sql_equipos = "SELECT nombre FROM estadios WHERE id = '".$id."';";
-		$rsEquipos = mysql_query($sql_equipos,$conn) or die(mysql_error());
-		if($rowEquipos = mysql_fetch_assoc($rsEquipos)){
-			return $rowEquipos["nombre"];
-		}else{
-			return "ERROR!";
-		}
-	}
-?>
+
 <?php if(!isset($_SESSION["id_usuario"])){ header("Location: log_in.php"); } ?>
 <?php require_once("includes/conversion.php"); ?>
 <?php include("templates/html_head.tpl.php"); ?>
